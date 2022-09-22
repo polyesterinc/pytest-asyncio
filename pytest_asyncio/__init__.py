3166d66dd7630cef9b71fc17f4c87a405360c2e2
Elias Freider <elias@freider.se>
2022-09-22 18:25:35 +0200

    Revert to yesterday's version

--- pytest_asyncio/__init__.py
+++ pytest_asyncio/__init__.py
@@ -1,5 +1,2 @@
 """The main point for importing pytest-asyncio items."""
-from ._version import version as __version__  # noqa
-from .plugin import fixture
-
-__all__ = ("fixture",)
+__version__ = "0.16.0dev0"
